91d8db0672a03000e9ecdcc0f4775b73b72df86a
juniork <abrahamkankola@gmail.com>
2022-10-31 12:58:13 +0100

    premier test unitaire prime

--- src/setup.test.jsx
+++ src/setup.test.jsx
@@ -5,6 +5,7 @@ import '@testing-library/jest-dom'
 
 
     test('renders component', () => {
+        console.log('premier test unitaire prime');
       render(<DashboardPage/>)
       const linkElement = screen.getByText(/Chatrooms/)
       expect(linkElement).toBeInTheDocument()
